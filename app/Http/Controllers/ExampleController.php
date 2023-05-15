d055683f4d53d1cf8f709d14fbc9608c9c4322cd
Lachezar <uzunov_89_lacho@abv.bg>
2023-05-15 10:34:48 +0300

    syncing files

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function homepage() {
+        $myName = 'Lacho';
+        $animals = ['cat', 'dog', 'lion'];
+
+        return view('homepage', ['allAnimals' => $animals, 'name' => $myName]);
+    }
+
+
+    public function aboutPage() {
+        return '<h1>About page</h1><a href="/">Go to home page</a>';
+    }
+}
